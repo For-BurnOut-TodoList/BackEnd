a1cb96d35ff0956588985d9acb7650779c238ab9
josoyoung <jjjj032321@gmail.com>
2025-12-13 14:10:16 +0900

    시크릿창에서도 열리게 고쳤어요

--- src/main/java/com/example/todolist/backend/config/SecurityConfig.java
+++ src/main/java/com/example/todolist/backend/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.todolist.backend.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -39,6 +40,7 @@ public class SecurityConfig {
                     return config;
                 }))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().permitAll()
                 );
